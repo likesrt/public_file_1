46ce3688ee3b40f68597ae48df9fc24c1b4445d4
yuyan <yuyan@broue.cn>
2024-12-09 22:24:02 +0800

    22

--- Script/auto_mount_disk.sh
+++ Script/auto_mount_disk.sh
@@ -3,121 +3,81 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 setup_path=/datadisk
-#if [ $1 != "" ];then
-	#setup_path=$1;
-#fi
 
-#检测磁盘数量
-sysDisk=`cat /proc/partitions|grep -v name|grep -v ram|awk '{print $4}'|grep -v '^$'|grep -v '[0-9]$'|grep -v 'vda'|grep -v 'xvda'|grep -v 'sda'|grep -e 'vd' -e 'sd' -e 'xvd'`
-if [ "${sysDisk}" == "" ]; then
-	echo -e "ERROR!This server has only one hard drive,exit"
-	echo -e "此服务器只有一块磁盘,无法挂载"
-	echo -e "Bye-bye"
-	exit;
+# 检测磁盘数量，如果只有一块硬盘，则退出
+sysDisk=$(cat /proc/partitions | grep -v name | grep -v ram | awk '{print $4}' | grep -v '^$' | grep -v '[0-9]$' | grep -v 'vda' | grep -v 'xvda' | grep -v 'sda' | grep -e 'vd' -e 'sd' -e 'xvd')
+if [ -z "$sysDisk" ]; then
+    echo -e "ERROR! This server has only one hard drive, exiting."
+    echo -e "此服务器只有一块磁盘, 无法挂载"
+    echo -e "Bye-bye"
+    exit 1
 fi
-#检测/datadisk目录是否已挂载磁盘
-mountDisk=`df -h | awk '{print $6}' |grep datadisk`
-if [ "${mountDisk}" != "" ]; then
-	echo -e "datadisk directory has been mounted,exit"
-	echo -e "datadisk目录已被挂载,不执行任何操作"
-	echo -e "Bye-bye"
-	exit;
+
+# 检测 /datadisk 目录是否已挂载磁盘，如果已挂载，则退出
+mountDisk=$(df -P | awk '{print $6}' | grep -w "$setup_path")
+if [ -n "$mountDisk" ]; then
+    echo -e "datadisk directory has already been mounted, exiting."
+    echo -e "datadisk目录已被挂载, 不执行任何操作"
+    echo -e "Bye-bye"
+    exit 1
 fi
-#检测是否有windows分区
-winDisk=`fdisk -l |grep "NTFS\|FAT32"`
-if [ "${winDisk}" != "" ];then
-	echo 'Warning: The Windows partition was detected. For your data security, Mount manually.';
-	echo "危险 数据盘为windwos分区，为了你的数据安全，请手动挂载，本脚本不执行任何操作。"
-	exit;
-fi
-echo "
-+----------------------------------------------------------------------
-| Bt-WebPanel Automatic disk partitioning tool
-+----------------------------------------------------------------------
-| Copyright © 2015-2017 BT-SOFT(http://www.bt.cn) All rights reserved.
-+----------------------------------------------------------------------
-| Auto mount partition disk to $setup_path
-+----------------------------------------------------------------------
-"
 
 
-#数据盘自动分区
-fdiskP(){
-	
-	for i in `cat /proc/partitions|grep -v name|grep -v ram|awk '{print $4}'|grep -v '^$'|grep -v '[0-9]$'|grep -v 'vda'|grep -v 'xvda'|grep -v 'sda'|grep -e 'vd' -e 'sd' -e 'xvd'`;
-	do
-		#判断指定目录是否被挂载
-		isR=`df -P|grep $setup_path`
-		if [ "$isR" != "" ];then
-			echo "Error: The $setup_path directory has been mounted."
-			return;
-		fi
-		
-		isM=`df -P|grep '/dev/${i}1'`
-		if [ "$isM" != "" ];then
-			echo "/dev/${i}1 has been mounted."
-			continue;
-		fi
-			
-		#判断是否存在未分区磁盘
-		isP=`fdisk -l /dev/$i |grep -v 'bytes'|grep "$i[1-9]*"`
-		if [ "$isP" = "" ];then
-				#开始分区
-				fdisk -S 56 /dev/$i << EOF
-n
-p
-1
 
 
-wq
-EOF
+# 数据盘自动分区并挂载
+fdiskP() {
+    # 遍历所有磁盘（除了已经挂载的）
+    for i in $(lsblk -d -o NAME | grep -E 'sd|vd'); do
+        echo "Processing /dev/$i"
 
-			sleep 5
-			#检查是否分区成功
-			checkP=`fdisk -l /dev/$i|grep "/dev/${i}1"`
-			if [ "$checkP" != "" ];then
-				#格式化分区
-				mkfs.ext4 /dev/${i}1
-				mkdir $setup_path
-				#挂载分区
-				sed -i "/\/dev\/${i}1/d" /etc/fstab
-				echo "/dev/${i}1    $setup_path    ext4    defaults    0 0" >> /etc/fstab
-				mount -a
-				df -h
-			fi
-		else
-			#判断是否存在Windows磁盘分区
-			isN=`fdisk -l /dev/$i|grep -v 'bytes'|grep -v "NTFS"|grep -v "FAT32"`
-			if [ "$isN" = "" ];then
-				echo 'Warning: The Windows partition was detected. For your data security, Mount manually.';
-				return;
-			fi
-			
-			#挂载已有分区
-			checkR=`df -P|grep "/dev/$i"`
-			if [ "$checkR" = "" ];then
-					mkdir $setup_path
-					sed -i "/\/dev\/${i}1/d" /etc/fstab
-					echo "/dev/${i}1    $setup_path    ext4    defaults    0 0" >> /etc/fstab
-					mount -a
-					df -h
-			fi
-			
-			#清理不可写分区
-			echo 'True' > $setup_path/checkD.pl
-			if [ ! -f $setup_path/checkD.pl ];then
-					sed -i "/\/dev\/${i}1/d" /etc/fstab
-					mount -a
-					df -h
-			else
-					rm -f $setup_path/checkD.pl
-			fi
-		fi
-	done
+        # 如果分区不存在，则创建分区
+        if ! fdisk -l /dev/$i | grep -q "${i}1"; then
+            echo "No partition found on /dev/$i, creating a new partition."
+            # 创建一个新的主分区并保存
+            echo -e "n\np\n1\n\n\nw" | fdisk /dev/$i
+            sleep 5
+        fi
+
+        # 格式化新分区
+        if [ ! -e "/dev/${i}1" ]; then
+            echo "Partition /dev/${i}1 does not exist. Skipping."
+            continue
+        fi
+
+        # 格式化分区
+        mkfs.ext4 /dev/${i}1
+
+        # 创建挂载目录
+        if [ ! -d "$setup_path" ]; then
+            mkdir -p "$setup_path"
+        fi
+
+        # 更新 /etc/fstab，确保不会重复添加
+        if ! grep -q "/dev/${i}1" /etc/fstab; then
+            echo "Adding /dev/${i}1 to /etc/fstab."
+            echo "/dev/${i}1    $setup_path    ext4    defaults    0 0" >> /etc/fstab
+        fi
+
+        # 挂载分区
+        mount /dev/${i}1 $setup_path
+        if [ $? -eq 0 ]; then
+            echo "/dev/${i}1 mounted successfully to $setup_path."
+        else
+            echo "Failed to mount /dev/${i}1."
+        fi
+
+        # 刷新文件系统
+        mount -a
+        df -h
+        break
+    done
 }
 
-lsblk
+# 调用分区和挂载函数
+fdiskP
+
+# 输出挂载状态
 echo -e ""
 echo -e "Done"
 echo -e "挂载成功"
-
